bf4dcfff2e14ffaca49f4b93c49037057d142a0e
anuragsharma1098 <anuragsharma1098@gmail.com>
2025-08-12 10:39:44 +0530

    q

--- java_selenium/src/java_ques/armstrong.java
+++ java_selenium/src/java_ques/armstrong.java
@@ -8,7 +8,7 @@ public class armstrong {
         System.out.println("Enter a number to check if it is an Armstrong number:");
         int number = sc.nextInt();
         sc.close();
-        //int number = 153; // Example number to check
+        // int number = 153; // Example number to check
         System.out.println("Original number is: " + number);
         if (isArmstrong(number)) {
             System.out.println(number + " is an Armstrong number.");
@@ -16,19 +16,32 @@ public class armstrong {
             System.out.println(number + " is not an Armstrong number.");
         }
     }
+    // public static boolean isArmstrong(int num) {
+    // int originalNum = num;
+    // int sum = 0;
+    // int digits = String.valueOf(num).length();
 
+    // while (num > 0) {
+    // int digit = num % 10;
+    // sum += Math.pow(digit, digits);
+    // num /= 10;
+    // }
+
+    // return sum == originalNum;
+    // }
     public static boolean isArmstrong(int num) {
-        int originalNum = num;
-        int sum = 0;
-        int digits = String.valueOf(num).length();
-
+        int onum = num;
+        int s = 0;
+        String str = String.valueOf(num);
+        int digits = str.length();
         while (num > 0) {
-            int digit = num % 10;
-            sum += Math.pow(digit, digits);
+            int d = num % 10;
+            s += Math.pow(d, digits);
             num /= 10;
         }
-
-        return sum == originalNum;
+        if (s == onum)
+            return true;
+        else
+            return false;
     }
-    
 }
--- java_selenium/src/java_ques/gcd_lcd.java
+++ java_selenium/src/java_ques/gcd_lcd.java
@@ -1,5 +1,28 @@
 package java_ques;
 
+import java.util.Scanner;
+
 public class gcd_lcd {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two numbers to find GCD and LCD:");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sc.close();
+        // Uncomment the next line to use hardcoded values for testing
+        // int a = 12, b = 15; // Example numbers
+        System.out.println("GCD of " + a + " and " + b + " is: " + gcd(a, b));
+        System.out.println("LCD of " + a + " and " + b + " is: " + lcd(a, b));
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    public static int lcd(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
 }
