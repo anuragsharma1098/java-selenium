05732718f09ce75bd87e6fa9177494ade4daa7d7
anuragsharma1098 <anuragsharma1098@gmail.com>
2025-08-19 19:54:05 +0530

    USER FUNCTIONS

--- java_selenium/src/java_revamp/day_4_5/oops_5_user.java
+++ java_selenium/src/java_revamp/day_4_5/oops_5_user.java
@@ -45,4 +45,77 @@ public class oops_5_user {
         this.address = address;
     }
 
+    // add new user method
+    public void addUser(int id, String name, int age, String address) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    // update user method
+    public void updateUser(int id, String name, int age, String address) {
+        if (this.id == id) {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        } else {
+            System.out.println("User ID not found.");
+        }
+    }
+
+    // delete user method
+    public void deleteUser(int id) {
+        if (this.id == id) {
+            this.id = 0;
+            this.name = null;
+            this.age = 0;
+            this.address = null;
+            System.out.println("User deleted successfully.");
+        } else {
+            System.out.println("User ID not found.");
+        }
+    }
+
+    // find user method
+    public oops_5_user findUser(int id) {
+        if (this.id == id) {
+            return this;
+        } else {
+            System.out.println("User ID not found.");
+            return null;
+        }
+    }
+
+    // display user method
+    public void displayUser() {
+        System.out.println("User ID: " + id);
+        System.out.println("User Name: " + name);
+        System.out.println("User Age: " + age);
+        System.out.println("User Address: " + address);
+        System.out.println("-------------------------");
+    }
+
+    // display all users method
+    public static void displayAllUsers(oops_5_user[] users) {
+        for (oops_5_user user : users) {
+            if (user != null) {
+                user.displayUser();
+            } else {
+                System.out.println("No user found at this index.");
+            }
+        }
+    }
+    // toString method for displaying user details
+
+    @Override
+    public String toString() {
+        return "\n" +
+                "  ID=      " + id + "\n" +
+                "  NAME=    " + name + "\n" +
+                "  AGE=     " + age + "\n" +
+                "  ADDRESS= " + address + "\n"
+                + "-------------------------";
+    }
+
 }
